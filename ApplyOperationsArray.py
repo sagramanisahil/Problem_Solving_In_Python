f61238047bd940478b1fce5601f52bccb29fab5e
Sahil Kumar <sagramanisahil@gmail.com>
2025-03-01 20:06:44 +0500

    Add ApplyOperationsArray

---
+++ ApplyOperationsArray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for i in range(n-1):
+            if(nums[i] == nums[i+1]):
+                nums[i] *= 2
+                nums[i+1] = 0
+        idx = 0
+
+        for i in range(n):
+            if nums[i] != 0:
+                nums[idx] = nums[i]
+                idx += 1
+        for i in range(idx, n):
+            nums[i] = 0
+        return nums
