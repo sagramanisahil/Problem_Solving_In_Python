82a9453404796fbd480ad457cd7980100c181618
Sahil Kumar <sagramanisahil@gmail.com>
2025-03-05 08:34:57 +0500

    Add CountTotalColoredCells

--- CountTotalColoredCells.py
+++ CountTotalColoredCells.py
@@ -1,11 +1,5 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        result = 1
-        i=0
-
-        while i < n:
-            result = result + (4*i)
-            i += 1
-        return result
+        return 1 + 2*(n-1) * n
 
         
