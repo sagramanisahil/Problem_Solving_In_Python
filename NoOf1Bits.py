5fed77dd87ca7b54cf6cb00d556b473c2b902779
Sahil Kumar <sagramanisahil@gmail.com>
2025-08-30 22:32:56 +0500

    Add NoOf1Bits

---
+++ NoOf1Bits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        for i in range(32):
+            if (n >> i) & 1:
+                count += 1
+        return count
